bad37d46062274c350f13ca4fd8cc76542a11670
shorinkiwata <fcl709@outlook.com>
2026-01-18 16:10:34 +0900

    fix

--- scripts/04g-caelestia-quickshell.sh
+++ scripts/04g-caelestia-quickshell.sh
@@ -98,7 +98,7 @@ exe pacman -Syu --needed --noconfirm fish
 section "Install" "Running Caelestia Installer"
 
 # Switch to user, go home, and run the installer
-if as_user fish $CAELESTIA_DIR/install.sh --noconfirm; then
+if as_user fish $CAELESTIA_DIR/install.fish --noconfirm; then
     success "Caelestia installation script completed."
 fi
 
